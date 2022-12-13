e41bbdf1cd339da8481281f3f037d340bb533779
Katsuomi Kobayashi <katsuomi.kobayashi@gmail.com>
2022-12-13 23:55:33 +0900

    Complete title and game scene loop

--- main.gd
+++ main.gd
@@ -1,14 +1,16 @@
 extends Node3D
 
+const TitleScene = preload("res://title.tscn")
+const GameScene = preload("res://game.tscn")
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	await $Title.game_started
-	print("Started!")
-	$Title.queue_free()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+	while true:
+		var title_scene = TitleScene.instantiate()
+		add_child(title_scene)
+		await title_scene.game_started
+		title_scene.queue_free()
+		
+		var game_scene = GameScene.instantiate()
+		add_child(game_scene)
+		await get_tree().create_timer(1).timeout
+		game_scene.queue_free()
