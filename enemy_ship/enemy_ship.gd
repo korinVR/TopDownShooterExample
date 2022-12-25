fbf530ad8856d4f425ad913bd8870589d9e39a49
Katsuomi Kobayashi <katsuomi.kobayashi@gmail.com>
2022-12-25 23:25:40 +0900

    Randomize enemy movement

--- enemy_ship/enemy_ship.gd
+++ enemy_ship/enemy_ship.gd
@@ -1,19 +1,23 @@
 extends RigidBody3D
 
-@export var acceleration: float
 @export var enemy_shot_speed: float
 
 const EnemyShot := preload("res://enemy_shot/enemy_shot.tscn")
 
 var shot_timer: float
 
+var acceleration: float
+var target_position_offset: Vector3
+
 func _ready() -> void:
 	shot_timer = randf_range(1, 2)
+	target_position_offset = Vector3(randf_range(-30, 30), 0, randf_range(-30, 30))
+	acceleration = randf_range(500000, 1500000)
 
 func _physics_process(delta: float) -> void:
 	if %Player == null:
 		return
-	var direction = (%Player.global_position - global_position).normalized()
+	var direction = (%Player.global_position + target_position_offset - global_position).normalized()
 	apply_force(direction * acceleration)
 	
 	$Model.look_at(%Player.global_position)
